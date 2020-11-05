8fb3a68b9cf03eb8c743661ecb347a51d55e6233
prekidac <al.slavisa@gmail.com>
2020-11-05 13:50:04 +0100

    func define

--- python/trening.py
+++ python/trening.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python3
-import json, os
+import sys, os
+import json
 
 config_file = "trening.config.json"
+
 class Trening(object):
     """
     docstring
@@ -10,8 +12,18 @@ class Trening(object):
         self.load_conf()
 
     def load_conf(self) -> None:
-        with open(os.path.join(os.getcwd(), config_file)) as f:
+        with open(os.path.join(os.path.dirname(sys.argv[0]), config_file)) as f:
             self.config = json.load(f)
+        
+    def determine_exercise(self) -> None:
+        pass
+
+    def print_exercise(self) -> None:
+        pass
+
+    def update_config(self) -> None:
+        pass
+
 
 if __name__ == "__main__":
     trening = Trening()
