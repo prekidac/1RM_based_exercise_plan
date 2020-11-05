d93b5aea8c860c1bfce06b5e83ed99b09a9334a1
prekidac <al.slavisa@gmail.com>
2020-11-05 17:43:46 +0100

    calculate weights

--- python/trening.py
+++ python/trening.py
@@ -6,20 +6,17 @@ import os
 from pathlib import Path
 import json
 
-if os.name == "posix":
-    config_file = ".config/trening.config.json"
-else:
-    raise OSError("Not implemented")
-
 
 class Trening(object):
 
-    def __init__(self) -> None:
+    def __init__(self, config_path) -> None:
+        self.config_path = config_path
         self.load_conf()
-        self.determine_exercise()
+        self.print_exercise()
+        self.update_config()
 
     def load_conf(self) -> None:
-        with open(os.path.join(Path.home(), config_file)) as f:
+        with open(config_path) as f:
             self.config = json.load(f)
 
     def determine_exercise(self) -> None:
@@ -34,9 +31,26 @@ class Trening(object):
             else:
                 self.current_cycle = self.config["cycle_order"][0]
             self.current_exercise = self.config["exercise_order"][0]
+        self.cycle_rms = self.config[self.current_cycle + "_RMs"]
+        self.one_rm = self.config["exercises"][self.current_exercise]["1RM"]
+
+    def calculate_set_weights(self) -> None:
+        self.weights = []
+        for rm in self.cycle_rms:
+            if self.current_exercise == "chin-up":
+                weight = self.config["percents"][rm] * ( self.one_rm + self.config["my_weight"] ) - self.config["my_weight"]
+                if weight < 0: weight = 0
+                weight = weight // self.config["weight_inc"] * self.config["weight_inc"]
+            else:
+                weight = self.config["percents"][rm] * self.one_rm
+                weight = weight // self.config["weight_inc"] * self.config["weight_inc"]
+            self.weights.append(weight)
 
 
     def print_exercise(self) -> None:
+        self.determine_exercise()
+        self.calculate_set_weights()
+        # print exercise
         pass
 
     def update_config(self) -> None:
@@ -44,4 +58,9 @@ class Trening(object):
 
 
 if __name__ == "__main__":
-    trening = Trening()
+    if os.name == "posix":
+        config_file = ".config/trening.config.json"
+    else:
+        raise OSError("Linux only")
+    config_path = os.path.join(Path.home(), config_file)
+    trening = Trening(config_path)
